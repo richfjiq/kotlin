0fc3ecf5c04e2c519054da8988a4ee2943b3f4b6
richfjiq <rfjiq1986@gmail.com>
2026-05-16 13:35:37 -0600

    kotlin project creation - variables

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/HelloWorld.kt
@@ -0,0 +1,9 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>){
+    println(printHello())
+}
+
+fun printHello(): String {
+    return "Hello Kotlin World!"
+}
---
+++ app/src/main/java/com/richdev/firstkotlinprogram/Variable.kt
@@ -0,0 +1,28 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    val a = 10
+    val b = 20
+    val c = a + b
+
+    println(c)
+
+    // var age = 10 // valid
+
+    // this is a single comment
+    /*
+    This is
+    a  multiple
+    comment line
+     */
+    /*
+    var _age = 20 // valid
+    var age3 = 30 // valid
+    */
+    var age = 20
+    age = 10
+    println(age)
+
+    val year = 2020
+    // year = 2019
+}
