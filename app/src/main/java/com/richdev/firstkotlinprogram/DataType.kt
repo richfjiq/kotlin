dd99a2f459a4700c8ddfb0b421287412b06072c3
richfjiq <rfjiq1986@gmail.com>
2026-05-16 19:16:34 -0600

    data types

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/DataType.kt
@@ -0,0 +1,21 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var a: Boolean = true
+    var b: Char = 'R'
+    var c: Byte = 12
+    var d: Short = -356
+    var e: Int = 43543
+    var f: Long = -23234235234L
+    var g: Float = 5.3485348F
+    var h: Double = 7.5345384658386
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
