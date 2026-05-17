071a0ef2d947d603bcc953a2e6c236a7acc4ee8c
richfjiq <rfjiq1986@gmail.com>
2026-05-17 17:24:41 -0600

    equality operators

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/EqualityOperators.kt
@@ -0,0 +1,13 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    val a: Int = 5
+    val b: Int = 5
+    println("a == b : ${a == b}")
+    println("a != b : ${a != b}")
+    println("a > b : ${a > b}")
+    println("a >= b : ${a >= b}")
+    println("a < b : ${a < b}")
+    println("a <= b : ${a <= b}")
+
+}
