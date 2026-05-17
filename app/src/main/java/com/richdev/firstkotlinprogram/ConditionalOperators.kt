461ba32af101fd44b97b1082730a865f4c19ec22
richfjiq <rfjiq1986@gmail.com>
2026-05-17 17:30:36 -0600

    conditional operators

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/ConditionalOperators.kt
@@ -0,0 +1,16 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var number1: Int = 5
+    var number2: Int = 8
+    var number3: Int = 12
+    var result: Boolean = false
+    // Conditional Operator ( && )
+    println("Conditional Operator ( && )")
+    result = (number1 > number2) && (number3 > number2)
+    println("Result: $result")
+    // Conditional Operator ( && )
+    println("Conditional Operator ( || )")
+    result = (number1 > number2) || (number3 > number2)
+    println("Result: $result")
+}
