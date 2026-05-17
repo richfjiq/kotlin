a3a6965d858b09dcbb54fd2316c67dcbcd819805
richfjiq <rfjiq1986@gmail.com>
2026-05-17 17:01:40 -0600

    arithmetic operators

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/ArithmeticOperators.kt
@@ -0,0 +1,15 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var number1: Double = 8.4
+    var number2: Double = 4.2
+    var result: Double = 0.0
+    result = number1 + number2
+    println("number1 + number2 = $result")
+    result = number1 - number2
+    println("number1 - number2 = $result")
+    result = number1 / number2
+    println("number1 / number2 = $result")
+    result = number1 % number2
+    println("number1 % number2 = $result")
+}
