a5371b9a2e0a87b7fa00c50c866a197f57a95cc3
richfjiq <rfjiq1986@gmail.com>
2026-05-19 08:16:09 -0600

    readLine

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/ReadLine.kt
@@ -0,0 +1,9 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    print("What is your name? ")
+    var name: String? = readLine()
+    print("How old are you? ")
+    var age: Int = readLine()!!.toInt()
+    println("Your name is $name and your age is $age")
+}
