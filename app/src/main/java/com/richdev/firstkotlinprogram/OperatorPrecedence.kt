2fee5a0675692220d4169b4159c1d7be42007bda
richfjiq <rfjiq1986@gmail.com>
2026-05-19 07:58:33 -0600

    perator precedence

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/OperatorPrecedence.kt
@@ -0,0 +1,14 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var result: Int = 5 + 2 * 4
+    println("Result = $result")
+    result = (5 + 2) * 4
+    println("Result = $result")
+    val x: Int = 8
+    var y: Int = 4
+    var z: Int = 2
+    var sum: Int = 0
+    sum = x + --y + --z
+    println("Sum = $sum")
+}
