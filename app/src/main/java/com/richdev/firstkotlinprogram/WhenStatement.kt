95d2c4ff67c002bb2551793bf130b282acc81417
richfjiq <rfjiq1986@gmail.com>
2026-05-19 09:02:19 -0600

    when statement

---
+++ app/src/main/java/com/richdev/firstkotlinprogram/WhenStatement.kt
@@ -0,0 +1,21 @@
+package com.richdev.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    print("Please enter a day number of the week : ")
+    var dayNumber: Int = readLine()!!.toInt()
+    var day: String
+
+    when(dayNumber)
+    {
+        1 -> day = "Monday"
+        2 -> day = "Tuesday"
+        3 -> day = "Wednesday"
+        4 -> day = "Thursday"
+        5 -> day = "Friday"
+        6 -> day = "Saturday"
+        7 -> day = "Sunday"
+        else -> day = "Invalid day choice"
+    }
+
+    println("Day is $day")
+}
